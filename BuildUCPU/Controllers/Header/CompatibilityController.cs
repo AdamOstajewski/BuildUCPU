46af96529467522f56624ef3f3e627053a7402be
AdamOstajewski <adam.ostajewski42@gmail.com>
2024-07-18 15:59:44 +0200

    git pull origin master

---
+++ BuildUCPU/Controllers/Header/CompatibilityController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuildUCPU.Controllers.Header
+{
+    public class CompatibilityController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
